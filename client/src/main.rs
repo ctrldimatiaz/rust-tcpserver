165c6e2301bb058de6dc79a4abb1fde8128b7a00
DiMatiaz <dimatiaz@gmail.com>
2026-08-29 21:19:17 +0100

    Add simple connection to client.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{io::AsyncWriteExt, net::TcpStream};
+
+#[tokio::main]
+async fn main() {
+    let mut connector = TcpStream::connect("127.0.0.1:6379").await.unwrap();
+
+    let buf = String::from("hello world");
+    let result = connector.write(buf.as_bytes()).await.unwrap();
+    println!("Sent {} and got {}", buf, result);
 }
