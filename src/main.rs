b471c231a52c11b7d1b8d0ee511cc9f69cf1c5b1
DiMatiaz <dimatiaz@gmail.com>
2026-08-29 20:37:40 +0100

    First try of tcp listener supposed to echoing incoming messages.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        println!("Accepting incoming connections at localhost:6379");
+
+        let (mut socket, _) = listener.accept().await.unwrap();
+
+        let mut buf = [0; 30];
+
+        let read_result = socket.read(&mut buf).await.unwrap();
+
+        println!("Read result {}", read_result);
+
+        let write_result = socket.write(&buf).await.unwrap();
+
+        println!("Read result {}", write_result);
+    }
 }
