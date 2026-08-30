aa1d607802dd35f62f630b3ffa8942e9c7d94ed2
DiMatiaz <dimatiaz@gmail.com>
2026-08-30 15:47:49 +0100

    Add logger and env variable read

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,6 @@
+use dotenv::dotenv;
+use env_logger::Env;
+use log::info;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpStream,
@@ -5,6 +8,13 @@ use tokio::{
 
 #[tokio::main]
 async fn main() {
+    // load .env
+    dotenv().ok();
+
+    let env = Env::default().filter_or("RUST_LOG", "info");
+
+    env_logger::init_from_env(env);
+
     let mut connector = TcpStream::connect("127.0.0.1:6379").await.unwrap();
 
     let message = String::from("hello world");
@@ -15,7 +25,7 @@ async fn main() {
 
     let _read_result = connector.read(&mut buf).await.unwrap();
 
-    println!(
+    info!(
         "Sent {} and got {}",
         message,
         String::from_utf8(buf.to_vec()).unwrap(),
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,6 @@
+use dotenv::dotenv;
+use env_logger::Env;
+use log::info;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpListener,
@@ -5,10 +8,17 @@ use tokio::{
 
 #[tokio::main]
 async fn main() {
+    // load .env
+    dotenv().ok();
+
+    let env = Env::default().filter_or("RUST_LOG", "info");
+
+    env_logger::init_from_env(env);
+
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
     loop {
-        println!("Accepting incoming connections at localhost:6379");
+        info!("Accepting incoming connections at localhost:6379");
 
         let (mut socket, _) = listener.accept().await.unwrap();
 
@@ -16,7 +26,7 @@ async fn main() {
 
         let _read_result = socket.read(&mut buf).await.unwrap();
 
-        println!("Read result {}", String::from_utf8(buf.to_vec()).unwrap());
+        info!("Read result {}", String::from_utf8(buf.to_vec()).unwrap());
 
         let _write_result = socket.write(&buf).await.unwrap();
     }
