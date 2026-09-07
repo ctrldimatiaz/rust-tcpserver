823c08f99db4ce4646d87a0d4febb2d54d91ae81
DiMatiaz <dimatiaz@gmail.com>
2026-09-07 16:15:59 +0100

    Fix parser imports. Add command parser implementation.

--- protocol/src/parser.rs
+++ protocol/src/parser.rs
@@ -1,6 +1,25 @@
-use crate::Command;
-use crate::ParseError;
+use crate::{Command, ParseError};
 
 pub fn parse_command(input: &str) -> Result<Command, ParseError> {
-    Ok(Command::Get(String::from("")))
+    let mut fields = input.split_whitespace();
+
+    match fields.next() {
+        Some("GET") => {
+            let key = fields.next().ok_or(ParseError::MissingArgument)?;
+
+            Ok(Command::Get(key.to_string()))
+        }
+        Some("SET") => {
+            let key = fields.next().ok_or(ParseError::MissingArgument)?;
+            let value = fields.next().ok_or(ParseError::MissingArgument)?;
+
+            Ok(Command::Set(key.to_string(), value.to_string()))
+        }
+        Some("DELETE") => {
+            let key = fields.next().ok_or(ParseError::MissingArgument)?;
+
+            Ok(Command::Delete(key.to_string()))
+        }
+        _ => Err(ParseError::InvalidCommand),
+    }
 }
